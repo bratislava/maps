dd760546c9af541356fc63267307993f9fc68881
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-07-18 15:11:39 +0200

    add special divider

--- libs/react-maps-ui/src/components/atoms/Divider/Divider.tsx
+++ libs/react-maps-ui/src/components/atoms/Divider/Divider.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import cx from "classnames";
+import { ReactComponent as SpecialDivider } from "../../../assets/special-divider.svg";
 
 export interface IDividerProps {
   className?: string;
+  isSpecial?: boolean;
 }
 
-export const Divider = ({ className }: IDividerProps) => {
-  return (
+export const Divider = ({ className, isSpecial = false }: IDividerProps) => {
+  return isSpecial ? (
+    <div>
+      <SpecialDivider className="w-full" />
+    </div>
+  ) : (
     <div
       className={cx(
         "h-[2px] bg-gray-lightmode dark:bg-gray-darkmode opacity-20",
