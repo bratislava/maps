93e12405a62cd79466e3aa596e68bc1622353435
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-08-01 11:56:06 +0200

    adjust cvicko map

---
+++ apps/cvicko-map/src/assets/layers/apollo/apollo-styles.ts
@@ -0,0 +1,32 @@
+const styles = [
+  {
+    id: "apollo-line-bg",
+    type: "line",
+    paint: {
+      "line-color": "#DA5F32",
+      "line-opacity": 1,
+      "line-width": [
+        "interpolate",
+        ["linear"],
+        ["zoom"],
+        // zoom is 11 (or less) -> line width will be 10px
+        11,
+        2,
+        // zoom is 20 (or greater) -> line width will be 50px
+        20,
+        24,
+      ],
+    },
+  },
+  {
+    id: "apollo-line-middle",
+    type: "line",
+    paint: {
+      "line-color": "#E5E5E5",
+      "line-opacity": 1,
+      "line-width": 1,
+    },
+  },
+];
+
+export default styles;
--- apps/cvicko-map/src/components/App.tsx
+++ apps/cvicko-map/src/components/App.tsx
@@ -1,8 +1,6 @@
-import { MouseEvent, useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { useTranslation } from "react-i18next";
+import { useEffect, useMemo, useRef, useState } from "react";
 import cx from "classnames";
 import "../styles.css";
-import { useResizeDetector } from "react-resize-detector";
 
 // maps
 import {
@@ -13,222 +11,71 @@ import {
   ThemeController,
   ViewportController,
   SlotType,
-  DISTRICTS_GEOJSON,
 } from "@bratislava/react-maps";
 
-import { Layer, useFilter, useCombinedFilter, Marker } from "@bratislava/react-mapbox";
-import { useArcgis } from "@bratislava/react-use-arcgis";
+import { Layer, useFilter } from "@bratislava/react-mapbox";
 
 // components
 import { Detail } from "./Detail";
 
 // layer styles
-import ESRI_STYLE from "../assets/layers/esri/esri";
-import DISTRICTS_STYLE from "../assets/layers/districts/districts";
+import apolloStyles from "../assets/layers/apollo/apollo-styles";
+import oldBridgeStyles from "../assets/layers/old-bridge/old-bridge-styles";
+import snpStyles from "../assets/layers/snp/snp-styles";
 
 // utils
-import mapboxgl, { MapboxGeoJSONFeature } from "mapbox-gl";
-import { processData, treeKindNameSkMappingObject } from "../utils/utils";
+import mapboxgl from "mapbox-gl";
 import { FeatureCollection } from "geojson";
-import { MobileHeader } from "./mobile/MobileHeader";
-import { MobileFilters } from "./mobile/MobileFilters";
-import { DesktopFilters } from "./desktop/DesktopFilters";
-import { MobileSearch } from "./mobile/MobileSearch";
-import { ILayerCategory } from "./Layers";
-import { Modal, Sidebar } from "@bratislava/react-maps-ui";
-import { Legend } from "./Legend";
 import { usePrevious } from "@bratislava/utils";
-
-const URL = "https://geoportal.bratislava.sk/hsite/rest/services/zp/STROMY/MapServer/0";
+import { getProcessedData } from "../utils/utils";
+import { useTranslation } from "react-i18next";
 
 export const App = () => {
   const { t } = useTranslation();
 
-  const [layerCategories, setLayerCategories] = useState<ILayerCategory[]>([]);
+  useEffect(() => {
+    document.title = t("title");
+  }, [t]);
 
   const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState<FeatureCollection | null>(null);
+  const [apolloData, setApolloData] = useState<FeatureCollection | null>(null);
+  const [oldBridgeData, setOldBridgeData] = useState<FeatureCollection | null>(null);
+  const [snpData, setSnpData] = useState<FeatureCollection | null>(null);
 
-  const { data: rawData } = useArcgis(URL, { pagination: false, format: "geojson" });
-
-  // USE STATE
-  const [uniqueYears, setUniqueYears] = useState<string[]>([]);
-  const [uniqueDistricts, setUniqueDistricts] = useState<string[]>([]);
-  const [uniqueSeasons, setUniqueSeasons] = useState<string[]>([]);
-  const [uniqueKinds, setUniqueKinds] = useState<string[]>([]);
-  const [uniqueLayers, setUniqueLayers] = useState<string[]>([]);
-
-  const [isSidebarVisible, setSidebarVisible] = useState<boolean | undefined>(undefined);
-  const [isLegendVisible, setLegendVisible] = useState<boolean | undefined>(undefined);
+  const [isDetailOpen, setDetailOpen] = useState<boolean | undefined>(undefined);
 
   useEffect(() => {
-    if (rawData) {
-      const { data, uniqueYears, uniqueKinds, uniqueDistricts, uniqueSeasons } =
-        processData(rawData);
+    const { apolloData, oldBridgeData, snpData } = getProcessedData();
 
-      setData(data);
-      setUniqueYears(uniqueYears);
-      setUniqueDistricts(uniqueDistricts);
-      setUniqueSeasons(uniqueSeasons);
-      setUniqueKinds(uniqueKinds);
-      setUniqueLayers(["planting", "substitute-planting"]);
-
-      setLayerCategories([
-        {
-          label: "Výsadba",
-          subLayers: ["planting"],
-        },
-        {
-          label: "Náhradná výsadba",
-          subLayers: ["substitute-planting"],
-        },
-      ]);
-      setLoading(false);
-    }
-  }, [rawData]);
+    setApolloData(apolloData);
+    setOldBridgeData(oldBridgeData);
+    setSnpData(snpData);
+    setLoading(false);
+  }, []);
 
   const mapRef = useRef<MapHandle>(null);
   mapboxgl.accessToken = import.meta.env.PUBLIC_MAPBOX_PUBLIC_TOKEN;
 
-  const [selectedFeatures, setSelectedFeatures] = useState<MapboxGeoJSONFeature[] | null>(null);
   const [isMobile, setMobile] = useState<boolean | null>(null);
-  const [isGeolocation, setGeolocation] = useState(false);
 
-  const previousSidebarVisible = usePrevious(isSidebarVisible);
   const previousMobile = usePrevious(isMobile);
 
-  const yearFilter = useFilter({
-    property: "year",
-    keys: uniqueYears,
+  const cvickoFilter = useFilter({
+    property: "cvicko",
+    keys: useMemo(() => ["apollo"], []),
   });
-
-  const districtFilter = useFilter({
-    property: "district",
-    keys: uniqueDistricts,
-  });
-
-  const seasonFilter = useFilter({
-    property: "season",
-    keys: uniqueSeasons,
-    defaultValues: useMemo(() => ({ spring: true, summer: true, autumn: true, winter: true }), []),
-  });
-
-  const layerFilter = useFilter({
-    property: "layer",
-    keepOnEmpty: true,
-    keys: uniqueLayers,
-    defaultValues: useMemo(
-      () => uniqueLayers.reduce((prev, curr) => ({ ...prev, [curr]: true }), {}),
-      [uniqueLayers],
-    ),
-  });
-
-  const kindFilter = useFilter({
-    property: "kindSimple",
-    keys: uniqueKinds,
-  });
-
-  const combinedFilter = useCombinedFilter({
-    combiner: "all",
-    filters: [
-      {
-        filter: yearFilter,
-        mapToActive: (activeYears) => ({
-          title: t("filters.year.title"),
-          items: activeYears,
-        }),
-      },
-      {
-        filter: districtFilter,
-        mapToActive: (activeDistricts) => ({
-          title: t("filters.district.title"),
-          items: activeDistricts,
-        }),
-      },
-      {
-        filter: kindFilter,
-        mapToActive: (activeKinds) => ({
-          title: t("filters.kind.title"),
-          items: activeKinds.map((activeKind) => treeKindNameSkMappingObject[activeKind]),
-        }),
-      },
-      {
-        onlyInExpression: true,
-        filter: layerFilter,
-        mapToActive: (activeLayers) => ({
-          title: t("filters.layer.title"),
-          items: activeLayers,
-        }),
-      },
-    ],
-  });
-
-  const closeDetail = useCallback(() => {
-    setSelectedFeatures([]);
-  }, []);
-
-  // close detailbox when sidebar is opened on mobile
-  useEffect(() => {
-    if (isMobile && isSidebarVisible == true && previousSidebarVisible == false) {
-      closeDetail();
-    }
-  }, [closeDetail, isMobile, isSidebarVisible, previousMobile, previousSidebarVisible]);
 
   // close sidebar on mobile and open on desktop
   useEffect(() => {
     // from mobile to desktop
     if (previousMobile !== false && isMobile === false) {
-      setSidebarVisible(true);
+      setDetailOpen(true);
     }
     // from desktop to mobile
     if (previousMobile !== true && isMobile === true) {
-      setSidebarVisible(false);
+      setDetailOpen(false);
     }
   }, [previousMobile, isMobile]);
-
-  const isDetailOpen = useMemo(
-    () => (selectedFeatures ? !!selectedFeatures.length : undefined),
-    [selectedFeatures],
-  );
-
-  const onLegendClick = useCallback((e: MouseEvent) => {
-    setLegendVisible((isLegendVisible) => !isLegendVisible);
-    e.stopPropagation();
-  }, []);
-
-  // fit to district
-  useEffect(() => {
-    districtFilter.activeKeys.length == 1
-      ? mapRef.current?.fitDistrict(districtFilter.activeKeys[0])
-      : mapRef.current?.changeViewport({
-          center: {
-            lat: 48.148598,
-            lng: 17.107748,
-          },
-          zoom: 10.75,
-        });
-  }, [districtFilter.activeKeys, mapRef]);
-
-  // move point to center when selected
-  useEffect(() => {
-    const MAP = mapRef.current;
-    if (MAP && selectedFeatures && selectedFeatures.length) {
-      const feature = selectedFeatures[0];
-      setTimeout(() => {
-        if (feature.geometry.type === "Point") {
-          mapRef.current?.changeViewport({
-            center: {
-              lng: feature.geometry.coordinates[0],
-              lat: feature.geometry.coordinates[1],
-            },
-          });
-        }
-      }, 0);
-    }
-  }, [selectedFeatures, mapRef]);
-
-  const { height: desktopDetailHeight, ref: desktopDetailRef } =
-    useResizeDetector<HTMLDivElement>();
 
   const viewportControllerSlots: SlotType = useMemo(() => {
     return isMobile
@@ -253,165 +100,58 @@ export const App = () => {
       }}
       isDevelopment={import.meta.env.DEV}
       isOutsideLoading={isLoading}
-      selectedFeatures={selectedFeatures ?? []}
       sources={{
-        ESRI_DATA: data,
-        DISTRICTS_GEOJSON,
+        apollo: apolloData,
+        oldBridge: oldBridgeData,
+        snp: snpData,
       }}
-      onFeaturesClick={setSelectedFeatures}
-      onMapClick={closeDetail}
       onMobileChange={setMobile}
-      onGeolocationChange={setGeolocation}
     >
-      <Layer filters={combinedFilter.expression} isVisible source="ESRI_DATA" styles={ESRI_STYLE} />
-      <Layer
-        ignoreClick
-        filters={districtFilter.expression}
-        source="DISTRICTS_GEOJSON"
-        styles={DISTRICTS_STYLE}
-      />
-
-      {!!selectedFeatures?.length && selectedFeatures[0].geometry.type === "Point" && (
-        <Marker
-          feature={{
-            type: "Feature",
-            geometry: {
-              type: "Point",
-              coordinates: selectedFeatures[0].geometry.coordinates,
-            },
-            properties: {},
-          }}
-          isRelativeToZoom
-        >
-          <div
-            className="w-10 h-10 bg-background-lightmode dark:bg-background-darkmode border-[6px] rounded-full"
-            style={{ borderColor: selectedFeatures[0].properties?.["color"] }}
-          ></div>
-        </Marker>
-      )}
+      <Layer source="apollo" styles={apolloStyles} />
+      <Layer source="oldBridge" styles={oldBridgeStyles} />
+      <Layer source="snp" styles={snpStyles} />
 
       <Slot name="controls">
-        <ThemeController
-          className={cx("fixed left-4 bottom-[88px] sm:bottom-8 sm:transform", {
-            "translate-x-96": isSidebarVisible && !isMobile,
-          })}
-        />
+        <ThemeController className="fixed left-4 bottom-[88px] sm:bottom-8 sm:transform" />
         <ViewportController
-          className="fixed right-4 bottom-[88px] sm:bottom-8"
+          className={cx("fixed right-4 bottom-[88px] sm:bottom-8 sm:transform", {
+            "-translate-x-96": isDetailOpen && !isMobile,
+          })}
           slots={viewportControllerSlots}
-          onLegendClick={onLegendClick}
         />
-        <MobileSearch mapRef={mapRef} mapboxgl={mapboxgl} isGeolocation={isGeolocation} />
       </Slot>
 
       <Layout isOnlyMobile>
-        <Slot name="mobile-header">
-          <MobileHeader
-            onFunnelClick={() => setSidebarVisible((isSidebarVisible) => !isSidebarVisible)}
-          />
-        </Slot>
-
-        <Slot name="mobile-filter" isVisible={isSidebarVisible} setVisible={setSidebarVisible}>
-          <MobileFilters
-            isVisible={isSidebarVisible}
-            setVisible={setSidebarVisible}
-            areFiltersDefault={combinedFilter.areDefault}
-            activeFilters={combinedFilter.active}
-            onResetFiltersClick={combinedFilter.reset}
-            yearFilter={yearFilter}
-            districtFilter={districtFilter}
-            seasonFilter={seasonFilter}
-            layerFilter={layerFilter}
-            layerCategories={layerCategories}
-            kindFilter={kindFilter}
-          />
-        </Slot>
-
         <Slot
           name="mobile-detail"
-          isVisible={isDetailOpen}
+          isVisible
           openPadding={{
             bottom: window.innerHeight / 2, // w-96 or 24rem
           }}
           avoidControls={false}
         >
-          <Detail isMobile features={selectedFeatures ?? []} onClose={closeDetail} />
-        </Slot>
-
-        <Slot
-          name="mobile-legend"
-          isVisible={isLegendVisible}
-          setVisible={setLegendVisible}
-          avoidControls={false}
-        >
-          <Sidebar
-            title={t("title")}
-            isVisible={isLegendVisible}
-            setVisible={setLegendVisible}
-            position="right"
-          >
-            <Legend />
-          </Sidebar>
+          <Detail
+            isMobile
+            isOpen={isDetailOpen ?? false}
+            onToggle={() => setDetailOpen(!isDetailOpen)}
+          />
         </Slot>
       </Layout>
 
       <Layout isOnlyDesktop>
-        <Slot
-          name="desktop-filters"
-          isVisible={isSidebarVisible}
-          setVisible={setSidebarVisible}
-          openPadding={{
-            left: 384, // w-96 or 24rem
-          }}
-        >
-          <DesktopFilters
-            mapboxgl={mapboxgl}
-            isVisible={isSidebarVisible}
-            setVisible={setSidebarVisible}
-            areFiltersDefault={combinedFilter.areDefault}
-            onResetFiltersClick={combinedFilter.reset}
-            mapRef={mapRef}
-            yearFilter={yearFilter}
-            districtFilter={districtFilter}
-            seasonFilter={seasonFilter}
-            layerFilter={layerFilter}
-            isGeolocation={isGeolocation}
-            layerCategories={layerCategories}
-            kindFilter={kindFilter}
-            filters={combinedFilter.expression}
-          />
-        </Slot>
-
         <Slot
           name="desktop-detail"
           isVisible={isDetailOpen}
           openPadding={{
             right: 384,
           }}
-          avoidControls={window.innerHeight <= (desktopDetailHeight ?? 0) + 200 ? true : false}
+          avoidControls
         >
-          <div
-            ref={desktopDetailRef}
-            className={cx(
-              "fixed top-0 right-0 w-96 bg-background-lightmode dark:bg-background-darkmode transition-all duration-500",
-              {
-                "translate-x-full": !isDetailOpen,
-                "shadow-lg": isDetailOpen,
-              },
-            )}
-          >
-            <Detail isMobile={false} features={selectedFeatures ?? []} onClose={closeDetail} />
-          </div>
-        </Slot>
-        <Slot name="desktop-legend">
-          <Modal
-            closeButtonInCorner
-            title={t("legend.title")}
-            isOpen={isLegendVisible ?? false}
-            onClose={() => setLegendVisible(false)}
-          >
-            <Legend />
-          </Modal>
+          <Detail
+            isMobile={false}
+            isOpen={isDetailOpen ?? false}
+            onToggle={() => setDetailOpen(!isDetailOpen)}
+          />
         </Slot>
       </Layout>
     </Map>
