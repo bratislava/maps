00194b6cea7c60555fb4e19a700a30f63ce9943d
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-08-01 12:38:26 +0200

    refactor getProcessedData function and add getCvickoFromQuery function

--- apps/cvicko-map/src/utils/utils.ts
+++ apps/cvicko-map/src/utils/utils.ts
@@ -1,49 +1,30 @@
-import { FeatureCollection, Feature } from "geojson";
-
 import { apolloRawData } from "../assets/layers/apollo/apollo-data";
 import { oldBridgeRawData } from "../assets/layers/old-bridge/old-bridge-data";
 import { snpRawData } from "../assets/layers/snp/snp-data";
+import { cvickoData } from "../assets/layers/cvicko/cvicko-data";
 
 export const getProcessedData = () => {
-  const apolloData: FeatureCollection = {
-    ...apolloRawData,
-    features: apolloRawData.features.map((feature) => {
-      return {
-        ...feature,
-        properties: {
-          ...feature.properties,
-        },
-      } as Feature;
-    }),
-  };
-
-  const oldBridgeData: FeatureCollection = {
-    ...oldBridgeRawData,
-    features: oldBridgeRawData.features.map((feature) => {
-      return {
-        ...feature,
-        properties: {
-          ...feature.properties,
-        },
-      } as Feature;
-    }),
-  };
-
-  const snpData: FeatureCollection = {
-    ...snpRawData,
-    features: snpRawData.features.map((feature) => {
-      return {
-        ...feature,
-        properties: {
-          ...feature.properties,
-        },
-      } as Feature;
-    }),
-  };
-
   return {
-    apolloData,
-    oldBridgeData,
-    snpData,
+    apolloData: apolloRawData,
+    oldBridgeData: oldBridgeRawData,
+    snpData: snpRawData,
+    cvickoData,
   };
+};
+
+type cvickoId = "apollo" | "lafranconi" | "most-snp" | "nabrezie" | "promenada" | "tyrsak";
+
+export const getCvickoIdFromQuery = (): cvickoId => {
+  const cvickoQuery = new URLSearchParams(window.location.search).get("cvicko");
+  switch (cvickoQuery) {
+    case "apollo":
+    case "lafranconi":
+    case "most-snp":
+    case "nabrezie":
+    case "promenada":
+    case "tyrsak":
+      return cvickoQuery;
+    default:
+      return "apollo";
+  }
 };
