c54970bd7d9394cbba828fcb3b4ce6ce53ef9984
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-08-01 17:46:17 +0200

    remove unused func

--- apps/cvicko-map/src/utils/utils.ts
+++ apps/cvicko-map/src/utils/utils.ts
@@ -1,17 +1,3 @@
-import { apolloRawData } from "../assets/layers/apollo/apollo-data";
-import { oldBridgeRawData } from "../assets/layers/old-bridge/old-bridge-data";
-import { snpRawData } from "../assets/layers/snp/snp-data";
-import { cvickoData } from "../assets/layers/cvicko/cvicko-data";
-
-export const getProcessedData = () => {
-  return {
-    apolloData: apolloRawData,
-    oldBridgeData: oldBridgeRawData,
-    snpData: snpRawData,
-    cvickoData,
-  };
-};
-
 type cvickoId = "apollo" | "lafranconi" | "most-snp" | "nabrezie" | "promenada" | "tyrsak";
 
 export const getCvickoIdFromQuery = (): cvickoId => {
