29950824d30c6e0f9e2c1776d7b33a596f02d431
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-08-08 10:06:52 +0200

    remove unused env

--- apps/sport-grounds-map/src/env.d.ts
+++ apps/sport-grounds-map/src/env.d.ts
@@ -5,7 +5,6 @@ interface ImportMetaEnv {
   readonly PUBLIC_MAPBOX_PUBLIC_TOKEN: string;
   readonly PUBLIC_MAPBOX_LIGHT_STYLE: string;
   readonly PUBLIC_MAPBOX_DARK_STYLE: string;
-  readonly PUBLIC_MAPBOX_SATELLITE_STYLE: string;
   readonly PUBLIC_MAPBOX_LAYER_PREFIX: string;
   // more env variables...
 }
