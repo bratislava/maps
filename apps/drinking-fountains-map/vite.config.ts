a68ca3d63b1fd11125fc708be49bdd4c1f89881e
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-08-25 14:59:24 +0200

    make base path relative

--- apps/drinking-fountains-map/vite.config.ts
+++ apps/drinking-fountains-map/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [react(), svgr(), checker({ typescript: true })],
   publicDir: "public",
   envPrefix: "PUBLIC_",
-  base: "/static-pages/drinking-fountains-map/",
+  base: "./",
 });
