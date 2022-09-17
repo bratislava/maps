ea77a82e85f1fc26a9406047680a1812e91f0364
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-09-17 11:02:46 +0200

    add build all command

---
+++ build-all.sh
@@ -0,0 +1,8 @@
+yarn workspace @bratislava/bratislava-for-everyone-map build && \
+yarn workspace @bratislava/closures-and-restrictions-map build && \
+yarn workspace @bratislava/cvicko-map build && \
+yarn workspace @bratislava/drinking-fountains-map build && \
+yarn workspace @bratislava/greenary-care-map build && \
+yarn workspace @bratislava/paas-map build && \
+yarn workspace @bratislava/planting-map build && \
+yarn workspace @bratislava/sport-grounds-map build
