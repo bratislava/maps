65c47c4f32924ee107ea35f8ecf10f80f8f6b036
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-10-13 17:04:48 +0200

    remove forgotten zone property

--- apps/paas-map/src/components/Detail/VisitorDetail.tsx
+++ apps/paas-map/src/components/Detail/VisitorDetail.tsx
@@ -38,7 +38,6 @@ export const VisitorDetail = ({ properties }: VisitorDetailProps) => {
   } = useTranslation("translation", { keyPrefix: "layers.visitors.detail" });
   return (
     <div className="flex flex-col justify-end w-full gap-4">
-      {properties.zone}
       <div className="font-semibold pb-1">{t("title")}</div>
       {properties.Status === "planned" && <div className="font-semibold">{t("planned")}</div>}
       <Row label={t("location")} text={properties["Nazov"]} />
