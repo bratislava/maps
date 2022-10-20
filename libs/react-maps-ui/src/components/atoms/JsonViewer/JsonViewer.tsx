8b51f0c3222866be73ed68e8f1e272c93abde827
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-10-20 16:45:03 +0200

    fix design of JsonViewer

--- libs/react-maps-ui/src/components/atoms/JsonViewer/JsonViewer.tsx
+++ libs/react-maps-ui/src/components/atoms/JsonViewer/JsonViewer.tsx
@@ -6,17 +6,20 @@ export interface IJsonViewerProps {
 
 export const JsonViewer = ({ json }: IJsonViewerProps) => {
   return (
-    <JSONPretty
-      data={json}
-      theme={{
-        main: "line-height:1.3;color:#6ce1fc;background:#222436;overflow:auto;padding:1rem;border-radius:0.5rem;",
-        error:
-          "line-height:1.3;color:#6ce1fc;background:#222436;overflow:auto;",
-        key: "color:#478dff;",
-        string: "color:#c3e88d;",
-        value: "color:#ff7747;",
-        boolean: "color:#ff7747;",
-      }}
-    />
+    <div>
+      <JSONPretty
+        className="overflow-hidden font-bold border-2 border-background-darkmode dark:border-gray-darkmode/20 rounded-lg"
+        data={json}
+        theme={{
+          main: "line-height:1.3;color:#6ce1fc;background:#333333;overflow:auto;padding:1rem;",
+          error:
+            "line-height:1.3;color:#6ce1fc;background:#333333;overflow:auto;",
+          key: "color:#478dff;",
+          string: "color:#c3e88d;",
+          value: "color:#ff7747;",
+          boolean: "color:#ff7747;",
+        }}
+      />
+    </div>
   );
 };
