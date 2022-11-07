febb37d5fabe9e6ba2840190deb4ea5b6d720cad
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-11-07 12:59:22 +0100

    code split

--- apps/non-residential-premises-map/vite.config.ts
+++ apps/non-residential-premises-map/vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
 import svgr from "vite-plugin-svgr";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react(), svgr(), checker({ typescript: true })],
@@ -10,5 +11,18 @@ export default defineConfig({
   base: "./",
   build: {
     chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+      output: {
+        manualChunks: {
+          "mapbox-gl": ["mapbox-gl"],
+          "framer-motion": ["framer-motion"],
+          "@bratislava/geojson-data": ["@bratislava/geojson-data"],
+          "@bratislava/react-maps": ["@bratislava/react-maps"],
+        },
+      },
+    },
   },
 });
