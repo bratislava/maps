6ec9f8c042e30728119bea2cfd2ef4297255dd5d
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-12-09 10:41:10 +0100

    split bundle

--- apps/homeless-people-help-map/vite.config.ts
+++ apps/homeless-people-help-map/vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
 import svgr from "vite-plugin-svgr";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react(), svgr(), checker({ typescript: true })],
@@ -10,5 +11,19 @@ export default defineConfig({
   base: "./",
   build: {
     chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      input: {
+        en: resolve(__dirname, "en.html"),
+        sk: resolve(__dirname, "sk.html"),
+      },
+      output: {
+        manualChunks: {
+          "mapbox-gl": ["mapbox-gl"],
+          "framer-motion": ["framer-motion"],
+          "@bratislava/geojson-data": ["@bratislava/geojson-data"],
+          "@bratislava/react-maps": ["@bratislava/react-maps"],
+        },
+      },
+    },
   },
 });
