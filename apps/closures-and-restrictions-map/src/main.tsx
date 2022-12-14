c6618848b139abe2e7ca1af865709a9309c31805
Matej Hospodár <matej.hospodar@bratislava.sk>
2022-12-14 09:07:26 +0100

    revert React mounting

--- apps/closures-and-restrictions-map/src/main.tsx
+++ apps/closures-and-restrictions-map/src/main.tsx
@@ -1,18 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { I18nextProvider } from "react-i18next";
 import App from "./components/App";
 import i18n from "./utils/i18n";
 import { I18nProvider } from "react-aria";
 
-const rootElement = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const rootElement = document.getElementById("root");
 
-rootElement.render(
-  <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <I18nProvider locale={i18n.language}>
-        <App />
-      </I18nProvider>
-    </I18nextProvider>
-  </React.StrictMode>,
-);
+if (!rootElement) {
+  console.error("No #root element...");
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <I18nextProvider i18n={i18n}>
+        <I18nProvider locale={i18n.language}>
+          <App />
+        </I18nProvider>
+      </I18nextProvider>
+    </React.StrictMode>,
+    rootElement,
+  );
+}
--- apps/greenary-care-map/src/main.tsx
+++ apps/greenary-care-map/src/main.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { I18nextProvider } from "react-i18next";
 import App from "./components/App";
 import i18n from "./utils/i18n";
 
-const rootElement = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const rootElement = document.getElementById("root");
 
-rootElement.render(
-  <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
-  </React.StrictMode>,
-);
+if (!rootElement) {
+  console.error("No #root element...");
+} else {
+  ReactDOM.render(
+    <React.StrictMode>
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
+    </React.StrictMode>,
+    rootElement,
+  );
+}
