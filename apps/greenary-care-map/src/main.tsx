de772960c16939a5b04a9bebc4c508b3f3f887a1
TomasSchaffer <119690180+TomasSchaffer@users.noreply.github.com>
2023-01-12 11:18:59 +0100

    Mp 34/upgrades maintanance (#29)
    * All packages updated
    
    * ReactDOM warnings fixed by updating main.tsx files
    
    Co-authored-by: Tomas Schaffer <schtomas@gmail.com>
    Co-authored-by: Martin Pinter <martin.pinter@vacuumlabs.com>

--- apps/greenary-care-map/src/main.tsx
+++ apps/greenary-care-map/src/main.tsx
@@ -1,20 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { I18nextProvider } from "react-i18next";
 import App from "./components/App";
 import i18n from "./utils/i18n";
 
-const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
-if (!rootElement) {
-  console.error("No #root element...");
-} else {
-  ReactDOM.render(
-    <React.StrictMode>
-      <I18nextProvider i18n={i18n}>
-        <App />
-      </I18nextProvider>
-    </React.StrictMode>,
-    rootElement,
-  );
-}
+root.render(
+  <React.StrictMode>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>
+);
