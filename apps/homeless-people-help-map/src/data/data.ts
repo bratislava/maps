133e1f7e3f3531aa9bed8e4b48d2f98262f74dea
Tomas Schaffer <schtomas@gmail.com>
2023-01-19 17:40:18 +0100

    Coordinates Updated

--- apps/homeless-people-help-map/src/data/data.ts
+++ apps/homeless-people-help-map/src/data/data.ts
@@ -991,7 +991,7 @@ export const data: FeatureCollection<Point> = {
     {
       id: 23,
       type: "Feature",
-      geometry: { type: "Point", coordinates: [17.2042377154266, 48.1388824759887] },
+      geometry: { type: "Point", coordinates: [17.089998498951914, 48.11294432175701] },
       properties: {
         layerName: "Služby pre ľudí bez domova",
         counseling_sk: "",
