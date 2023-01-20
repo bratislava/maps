689458da8f7de81def070860c7062d38ae6f900f
Martin Pinter <martin.pinter@bratislava.sk>
2023-01-20 12:11:32 +0100

    Fix global build, remove react-mapbox-docs app

--- apps/react-mapbox-docs/src/examples/DisplayMap.tsx
+++
@@ -1,9 +0,0 @@
-import { Mapbox } from '@bratislava/react-mapbox';
-
-export const DisplayMap = () => {
-  return (
-    <div style={{ width: '100%', height: '300px' }}>
-      <Mapbox mapboxAccessToken={import.meta.env.PUBLIC_MAPBOX_PUBLIC_TOKEN} />
-    </div>
-  );
-};
--- libs/utils/src/hooks/useEffectDebugger.ts
+++ libs/utils/src/hooks/useEffectDebugger.ts
@@ -33,8 +33,14 @@ export const useEffectDebugger = (
           {} as { [key: string]: { before: unknown; after: unknown } }
         )
       : {};
+  
+  // for some reason we need this retype, even if inferred type should be the same ?
+  // likely it uses the vite augmented type if used this way but infers the node type if we use just import.meta
+  // was not able to explicitly import this type though - so if this breaks, find a way to do it
+  // alternatively, find what is this for and find a different way to do it
+  const importMeta = import.meta as ImportMeta
 
-  if (Object.keys(changedDeps).length && import.meta.env.DEV) {
+  if (Object.keys(changedDeps).length && importMeta?.env?.DEV) {
     console.log(name ?? "[use-effect-debugger]", changedDeps);
   }
 
