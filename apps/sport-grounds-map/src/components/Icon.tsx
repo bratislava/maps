1c343caa311ac8e9ca1c59a7c2e64bb206473b38
TomasSchaffer <119690180+TomasSchaffer@users.noreply.github.com>
2023-04-26 21:39:10 +0200

    Selected Icons Fixed (#100)
    Co-authored-by: Tomas Schaffer <schtomas@gmail.com>

--- apps/sport-grounds-map/src/components/Icon.tsx
+++ apps/sport-grounds-map/src/components/Icon.tsx
@@ -23,14 +23,27 @@ export const icons = [
 export interface IIconProps {
   icon?: typeof icons[number]["name"];
   size?: number;
+  isActicve?: boolean;
 }
 
-export const Icon = ({ icon, size = 24 }: IIconProps) => {
+export const Icon = ({ icon, size = 24, isActicve = false }: IIconProps) => {
   const IconSvgComponent = icons.find((i) => i.name === icon)?.component;
 
+  const svgBorder = !isActicve ? "" : 'border-8 border-primary-soft';
+
+  const svgBackground: string = !isActicve
+    ? `bg-primary text-primary`
+    : `!text-primary ` + "bg-white z-50";
+
   return IconSvgComponent ? (
-    <IconSvgComponent width={size} height={size} />
+    <div
+      className={`relative transform active:scale-75 transition-transform cursor-pointer w-fit h-fit rounded-full text-white flex items-center justify-center  ${svgBackground}  ${svgBorder}`}
+    >
+      <IconSvgComponent width={size} height={size} />
+    </div>
   ) : (
-    <OtherIcon width={size} height={size} />
+    <></>
   );
+
+
 };
