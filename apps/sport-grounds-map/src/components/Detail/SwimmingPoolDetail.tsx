a0eaa3cc3db82dbd6ab4b9bd04dd3062324a9eeb
Tomas Schaffer <schtomas@gmail.com>
2023-05-16 18:11:28 +0200

    Detail Button Position Set

--- apps/sport-grounds-map/src/components/Detail/SwimmingPoolDetail.tsx
+++ apps/sport-grounds-map/src/components/Detail/SwimmingPoolDetail.tsx
@@ -68,7 +68,7 @@ export const SwimmingPoolDetail = ({ feature, isMobile }: SwimmingPoolDetailProp
       <>
         <Image src={imgSrc} isMobile={isMobile} />
 
-        <div className="absolute top-[232px] left-6">
+        <div className="absolute top-[232px] left-4">
           <a
             href={feature.properties?.["buyOnline"]}
             target="_blank"
