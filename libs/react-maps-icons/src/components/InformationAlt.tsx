4de1bdbd973c09ff018c08216a5681cd14b4c985
TomasSchaffer <119690180+TomasSchaffer@users.noreply.github.com>
2023-05-16 17:59:51 +0200

    Icons fixed (#107)
    Co-authored-by: Tomas Schaffer <schtomas@gmail.com>

--- libs/react-maps-icons/src/components/InformationAlt.tsx
+++ libs/react-maps-icons/src/components/InformationAlt.tsx
@@ -10,18 +10,9 @@ export const InformationAlt = ({
 
   return (
     <div className={className}>
-      <svg
-        width={24}
-        height={24}
-        stroke="currentColor"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          stroke="currentColor"
-          d="M12.001 8.61c-.62 0-1.13-.51-1.13-1.13 0-.62.51-1.13 1.13-1.13.62 0 1.13.51 1.13 1.13 0 .62-.51 1.13-1.13 1.13ZM12.75 10.26h-1.5v6.64h1.5v-6.64Z"
-          fill="currentColor"
-        />
+      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12.0011 8.6101C11.3811 8.6101 10.8711 8.1001 10.8711 7.4801C10.8711 6.8601 11.3811 6.3501 12.0011 6.3501C12.6211 6.3501 13.1311 6.8601 13.1311 7.4801C13.1311 8.1001 12.6211 8.6101 12.0011 8.6101Z" fill="currentColor" />
+        <path d="M12.75 10.2598H11.25V16.8998H12.75V10.2598Z" fill="currentColor" />
       </svg>
     </div>
   );
