96adcb9a35688cf8f35348e98f5ed9260f684f33
Kubo Hrubý <123157589+KuboHBA@users.noreply.github.com>
2023-05-16 14:32:20 +0200

    Icon change for Areál zdravia Zlaté piesky, from pool to water

--- apps/sport-grounds-map/src/utils/utils.ts
+++ apps/sport-grounds-map/src/utils/utils.ts
@@ -27,7 +27,7 @@ export const processData = () => {
       ...rawDataPools.features.map((feature) => {
         GLOBAL_ID++;
         const layer = "swimmingPools";
-        const icon = "pool";
+        const icon = feature.properties["Nazov_SK"] === "Areál zdravia Zlaté piesky" ? "water" : "pool";
         return {
           id: GLOBAL_ID,
           ...feature,
@@ -41,6 +41,7 @@ export const processData = () => {
       }),
     ],
   });
+}
 
   const uniqueDistricts: string[] = getUniqueValuesFromFeatures(data.features, "district");
   const uniqueTypes: string[] = getUniqueValuesFromFeatures(data.features, "tags");
