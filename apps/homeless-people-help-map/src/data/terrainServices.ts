334fa97dbf659e4a78cb01cb1951ee120a9ea417
Martin Pinter <martin.pinter@bratislava.sk>
2023-08-23 16:03:46 +0200

    Fix terenne sluzby

--- apps/homeless-people-help-map/src/data/terrainServices.ts
+++ apps/homeless-people-help-map/src/data/terrainServices.ts
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next";
 import { featureCollection } from "@turf/helpers";
 import { DISTRICTS_GEOJSON } from "@bratislava/geojson-data";
 import { ITerrainService } from "../components/Layers";
+import { regions } from "./regions";
 
 export const useTerrainServices = () => {
   const { i18n } = useTranslation();
@@ -12,24 +13,29 @@ export const useTerrainServices = () => {
     data:
       data &&
       // map to format of old data
-      data?.terenneSluzbies?.data.map(
-        (service) =>
-          ({
-            key: service.id,
-            title: service.attributes?.Nazov,
-            provider: service.attributes?.Poskytovatel,
-            phone: service.attributes?.Telefon,
-            available: service.attributes?.Dostupnost,
-            price: service.attributes?.Cena_kapacita,
-            geojson: featureCollection(
-              DISTRICTS_GEOJSON.features.filter((feature) =>
-                service.attributes?.Lokalita_posobnosti?.split(",").includes(
-                  feature.properties.name,
-                ),
-              ),
-            ),
-          } as ITerrainService),
-      ),
+      data?.terenneSluzbies?.data.map((service) => {
+        return {
+          key: service.id,
+          title: service.attributes?.Nazov,
+          provider: service.attributes?.Poskytovatel,
+          phone: service.attributes?.Telefon,
+          available: service.attributes?.Dostupnost,
+          price: service.attributes?.Cena_kapacita,
+          geojson: featureCollection([
+            ...DISTRICTS_GEOJSON.features.filter((feature) => {
+              return service.attributes?.Lokalita_posobnosti?.split(",")
+                .map((s) => s.trim())
+                .includes(feature.properties.name.trim());
+            }),
+            ...regions.features.filter((feature) => {
+              if (!feature?.properties?.name) return;
+              return service.attributes?.Lokalita_posobnosti?.split(",")
+                .map((s) => s.trim())
+                .includes(feature.properties.name.trim());
+            }),
+          ]),
+        } as ITerrainService;
+      }),
     isLoading,
     error,
   };
