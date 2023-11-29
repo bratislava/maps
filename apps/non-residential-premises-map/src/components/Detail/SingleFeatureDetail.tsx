f91e46cfa73ddd51b375af15e84950e28f814b13
Miloš Medo <milos.medo2@gmail.com>
2023-11-29 18:25:24 +0000

    Fixed locality on the mobile device detail modal

--- apps/non-residential-premises-map/src/components/Detail/SingleFeatureDetail.tsx
+++ apps/non-residential-premises-map/src/components/Detail/SingleFeatureDetail.tsx
@@ -51,7 +51,7 @@ export const SingleFeatureDetail = ({ feature, isExpanded }: ISingleFeatureDetai
 
       {!isExpanded && (
         <div className="py-4 px-6">
-          <DataDisplay label={t("locality")} text={feature.properties?.lessee} />
+          <DataDisplay label={t("locality")} text={feature.properties?.locality} />
         </div>
       )}
       {isExpanded && <DetailDataDisplay isSingleFeature={true} feature={feature} />}
