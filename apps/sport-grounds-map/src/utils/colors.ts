f8481c9a1485c92ff8ceb7007d34d7168a5cc981
Adam Grund <adam.grund@gmail.com>
2024-06-10 10:56:37 +0200

    [bratislava/kupaliska-starz-be#135] icons color change (#186)

--- apps/sport-grounds-map/src/utils/colors.ts
+++ apps/sport-grounds-map/src/utils/colors.ts
@@ -1,6 +1,6 @@
 export const colors = {
-    purpleLight: "#5158D8",
-    purple: "#07038C",
-    purpleSoft: "#aeadcf",
-    azure: "#D0ECF8"
-}
+  purpleLight: "#5158D8",
+  purple: "#0A07FE",
+  purpleSoft: "#aeadcf",
+  azure: "#D0ECF8",
+};
