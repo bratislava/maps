0601169c4c536ec6b8c0fd518753f159176b2de8
Martin Pinter <mpinter@users.noreply.github.com>
2024-12-02 16:25:40 +0100

    More paas udr fixes (#196)

--- apps/paas-map/src/components/Detail/VisitorDetail.tsx
+++ apps/paas-map/src/components/Detail/VisitorDetail.tsx
@@ -7,22 +7,22 @@ import { ButtonLink } from "../ButtonLink";
 import { Row } from "./Row";
 
 export const visitorPropertiesSchema = z.object({
-  Nazov: z.string(),
-  UDR_ID: z.number().nullable(),
-  Informacia_RPK_sk: z.string().nullable().optional(),
-  Informacia_RPK_en: z.string().nullable().optional(),
-  Informacia_NPK_sk: z.string().nullable().optional(),
-  Informacia_NPK_en: z.string().nullable().optional(),
-  Cas_spoplatnenia_sk: z.string().nullable().optional(),
-  Cas_spoplatnenia_en: z.string().nullable().optional(),
-  Zakladna_cena: z.number().nullable().optional(),
+  nazov: z.string(),
+  udr_id: z.number().nullable(),
+  informacia_rpk_sk: z.string().nullable().optional(),
+  informacia_rpk_en: z.string().nullable().optional(),
+  informacia_npk_sk: z.string().nullable().optional(),
+  informacia_npk_en: z.string().nullable().optional(),
+  cas_spoplatnenia_sk: z.string().nullable().optional(),
+  cas_spoplatnenia_en: z.string().nullable().optional(),
+  zakladna_cena: z.number().nullable().optional(),
   vikendy_a_sviatky: z.number().nullable().optional(),
-  Doplnkova_informacia_sk: z.string().nullable().optional(),
-  Doplnkova_informacia_en: z.string().nullable().optional(),
-  Vyhradene_sk: z.string().nullable().optional(),
-  Vyhradene_en: z.string().nullable().optional(),
-  Kod_rezidentskej_zony: z.string().nullable().optional(),
-  Status: z.string(),
+  doplnkova_informacia_sk: z.string().nullable().optional(),
+  doplnkova_informacia_en: z.string().nullable().optional(),
+  vyhradene_park_statie_sk: z.string().nullable().optional(),
+  vyhradene_park_statie_en: z.string().nullable().optional(),
+  kod_rezidentskej_zony: z.string().nullable().optional(),
+  status: z.string(),
   zone: z.string().optional(),
 });
 
@@ -40,19 +40,19 @@ export const VisitorDetail = ({ properties }: VisitorDetailProps) => {
   return (
     <div className="flex flex-col justify-end w-full gap-4 sm:pb-4">
       <div className="font-semibold pb-1">{t("title")}</div>
-      {properties.Status === "planned" && <div className="font-semibold">{t("planned")}</div>}
-      <Row label={t("location")} text={properties["Nazov"]} />
-      <Row label={t("parkingSectionCode")} text={`${properties["UDR_ID"]}`} />
+      {properties.status === "planned" && <div className="font-semibold">{t("planned")}</div>}
+      <Row label={t("location")} text={properties["nazov"]} />
+      <Row label={t("parkingSectionCode")} text={`${properties["udr_id"]}`} />
       <Row
         label={t("chargingTime")}
         text={
-          language === "sk" ? properties["Cas_spoplatnenia_sk"] : properties["Cas_spoplatnenia_en"]
+          language === "sk" ? properties["cas_spoplatnenia_sk"] : properties["cas_spoplatnenia_en"]
         }
       />
       <Row
         label={t("price")}
         text={
-          properties["Zakladna_cena"] ? `${properties["Zakladna_cena"]?.toFixed(2)} € / h` : null
+          properties["zakladna_cena"] ? `${properties["zakladna_cena"]?.toFixed(2)} € / h` : null
         }
       />
       <Row
@@ -64,30 +64,30 @@ export const VisitorDetail = ({ properties }: VisitorDetailProps) => {
         }
       />
 
-      {(properties["Informacia_RPK_sk"] ||
-        properties["Informacia_NPK_sk"] ||
-        properties["Doplnkova_informacia_sk"]) && (
+      {(properties["informacia_rpk_sk"] ||
+        properties["informacia_npk_sk"] ||
+        properties["doplnkova_informacia_sk"]) && (
         <Note className="flex flex-col gap-4 bg-primary/20 dark:bg-primary/20">
-          {properties["Informacia_RPK_sk"] && language === "sk" && (
-            <div className="font-light">{properties["Informacia_RPK_sk"]}</div>
+          {properties["informacia_rpk_sk"] && language === "sk" && (
+            <div className="font-light">{properties["informacia_rpk_sk"]}</div>
           )}
 
-          {properties["Informacia_RPK_en"] && language === "en" && (
-            <div className="font-light">{properties["Informacia_RPK_en"]}</div>
+          {properties["informacia_rpk_en"] && language === "en" && (
+            <div className="font-light">{properties["informacia_rpk_en"]}</div>
           )}
 
-          {properties["Informacia_NPK_sk"] && language === "sk" && (
-            <div className="font-light">{properties["Informacia_NPK_sk"]}</div>
+          {properties["informacia_npk_sk"] && language === "sk" && (
+            <div className="font-light">{properties["informacia_npk_sk"]}</div>
           )}
 
-          {properties["Informacia_NPK_en"] && language === "en" && (
-            <div className="font-light">{properties["Informacia_NPK_en"]}</div>
+          {properties["informacia_npk_en"] && language === "en" && (
+            <div className="font-light">{properties["informacia_npk_en"]}</div>
           )}
-          {properties["Doplnkova_informacia_sk"] && language === "sk" && (
+          {properties["doplnkova_informacia_sk"] && language === "sk" && (
             <div className="font-light">
-              {properties["Doplnkova_informacia_sk"]}{" "}
-              {(properties["Doplnkova_informacia_sk"]?.includes("bonus") ||
-                properties["Doplnkova_informacia_sk"]?.includes("Bonus")) && (
+              {properties["doplnkova_informacia_sk"]}{" "}
+              {(properties["doplnkova_informacia_sk"]?.includes("bonus") ||
+                properties["doplnkova_informacia_sk"]?.includes("Bonus")) && (
                 <Popover
                   allowedPlacements={["bottom", "top", "left"]}
                   button={({ toggle }) => (
@@ -119,11 +119,11 @@ export const VisitorDetail = ({ properties }: VisitorDetailProps) => {
               )}
             </div>
           )}
-          {properties["Doplnkova_informacia_en"] && language === "en" && (
+          {properties["doplnkova_informacia_en"] && language === "en" && (
             <div className="font-light">
-              {properties["Doplnkova_informacia_en"]}{" "}
-              {(properties["Doplnkova_informacia_en"]?.includes("bonus") ||
-                properties["Doplnkova_informacia_en"]?.includes("Bonus")) && (
+              {properties["doplnkova_informacia_en"]}{" "}
+              {(properties["doplnkova_informacia_en"]?.includes("bonus") ||
+                properties["doplnkova_informacia_en"]?.includes("Bonus")) && (
                 <Popover
                   allowedPlacements={["bottom", "top", "left"]}
                   button={({ toggle }) => (
