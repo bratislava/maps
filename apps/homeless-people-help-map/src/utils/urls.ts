93af04002dc812269314f56948271b303e86342d
Adam Grund <adam.grund@gmail.com>
2025-09-26 11:39:46 +0200

    add nest proxy to PUBLIC_TOILETS_URL (homeless-people-help-map) (#235)

--- apps/homeless-people-help-map/src/utils/urls.ts
+++ apps/homeless-people-help-map/src/utils/urls.ts
@@ -1,2 +1,2 @@
 export const PUBLIC_TOILETS_URL =
-  "https://geoportal.bratislava.sk/hSite/rest/services/Hosted/verejne_toalety_data_/FeatureServer/1";
+  "https://nest-proxy.bratislava.sk/geoportal/hSite/rest/services/Hosted/verejne_toalety_data_/FeatureServer/1";
